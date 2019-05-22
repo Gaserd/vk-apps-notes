d1c05925693861a64770921842d6dfaef6ab94d1
gaserd <gaserd123@gmail.com>
2019-05-22 19:40:15 +0300

    Updates

---
+++ precache-manifest.d1626a65a8b81b72d610382cac8c2f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409138060abf75a5a2a7609f966425d9",
+    "url": "/vk-apps-notes/index.html"
+  },
+  {
+    "revision": "6b5ff2b32a5826de3dc0",
+    "url": "/vk-apps-notes/static/css/2.b5769064.chunk.css"
+  },
+  {
+    "revision": "5c04116a3ff0e8cceadf",
+    "url": "/vk-apps-notes/static/css/main.c793b830.chunk.css"
+  },
+  {
+    "revision": "6b5ff2b32a5826de3dc0",
+    "url": "/vk-apps-notes/static/js/2.4201e702.chunk.js"
+  },
+  {
+    "revision": "5c04116a3ff0e8cceadf",
+    "url": "/vk-apps-notes/static/js/main.92365c2b.chunk.js"
+  },
+  {
+    "revision": "63b0ecff2a755f726a4c",
+    "url": "/vk-apps-notes/static/js/runtime~main.87ffbf5b.js"
+  }
+]);
